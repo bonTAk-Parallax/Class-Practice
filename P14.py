2dade1eca0b2997c22b85bdffc5d2b99c6bc25a7
bonTAk-Parallax <tanishup119@gmail.com>
2026-01-15 10:24:09 +0545

    sum of 3 nums that equal to zero successfully filtered from the list

--- P14.py
+++ P14.py
@@ -2,17 +2,39 @@
 class operate:
     tlst =  [-25, -10, -7, -3, 2, 4, 8, 10]
     def op(self):
-        lst = self.tlst
+        lst = self.tlst.copy()
         i = 0
-        while i<len(lst)-1:
-            print(lst[i])
-            i += 1
+        tdct = dict()
+        tlst = []
+        while i<len(lst):
+            print(f"working on: {lst[i]}")
             j = 0
-            while j<len(lst):
-                if lst[j]!= lst[i]:
-                    print(lst[j])
+            while j<len(lst)-1:
+                temp = self.tlst[:]
+                temp.remove(lst[i])
+                print(f"Removing {lst[i]}, lst: {temp}")
+                print(f"working on j: {temp[j]}")
+                current = temp[j]
+                sum = lst[i]+temp[j]
+                temp.remove(temp[j])
+                print(f"lst: {temp}")
+                k = 0
+                while k<len(temp):
+                    print(f"working on k: {temp[k]}")
+                    if sum + temp[k] == 0:
+                        tlst.append(current)
+                        tlst.append(temp[k])
+                        tdct.update({lst[i]:tlst})
+                        print(f"one zero from i={lst[i]}, j={current}, k={temp[k]}")
+                        tlst = []
+                    k += 1
+                print()
                 j += 1
+            i += 1
 
-
+        print(f"Final numbers whose sum = 0:")
+        for k,v in tdct.items():
+            result = [k, v[0], v[1]]
+            print(result)
 ob = operate()
 ob.op()
